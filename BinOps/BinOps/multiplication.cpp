01f6b586dc216613349dc8f6a99d19ae58aa87b8
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-02 19:05:10 +0100

    Finished multiplication

--- BinOps/BinOps/multiplication.cpp
+++ BinOps/BinOps/multiplication.cpp
@@ -2,14 +2,11 @@
 
 int bitwiseMult(int a, int b) {
   // Made by me, works with negatives and overflows, and less loops
-  int carry_over{}, placeholder{}, result{};
-  carry_over = (~a) & b;
-  result     = a ^ b;
-  while (carry_over != 0) {
-    carry_over <<= 1;
-    placeholder = (~result) & carry_over;
-    result ^= carry_over;
-    carry_over = placeholder;
+  int result{};
+  for (int position = 0; position < static_cast<int>(sizeof(int) * 8); position++) {
+    if (((b >> position) & 1) == 1) {
+      result = bitwiseAdd(result, a << position);
+    }
   }
   return result;
 }
