4504bed79b9e847c170d0b0d18d949d6c686ecb3
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-12 18:57:42 +0100

    New solutions and size_t problems

--- BinOps/BinOps/binSearch1.hpp
+++ BinOps/BinOps/binSearch1.hpp
@@ -1,3 +1,4 @@
 #pragma once
+// #include <stdlib.h>
 #include <vector>
 std::vector<int> searchRange(std::vector<int> nums, int target);
---
+++ Others/Others/treeDiff.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+	
+};
+int maxAncestorDiff(TreeNode* root, std::vector<int>& stack);
