6f9d50e14ebffb8d712582ec1a5b46ceb41bb387
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-24 22:46:47 +0100

    More solutions

--- DynamicProgramming/DynamicProgramming/candidateCombSum2.cpp
+++ DynamicProgramming/DynamicProgramming/candidateCombSum2.cpp
@@ -1,7 +1,7 @@
 #include "candidateCombSum2.hpp"
 
 std::vector<std::vector<int>> combinationSum2(std::vector<int> candidates, int target) {
-    // std::sort(candidates.begin(), candidates.end());
+    // Only works with LLVM compiler (too smart man)
     std::unordered_map<int, int> map;
     for (int num : candidates) map[num]++;
     std::vector<std::vector<std::vector<int>>> dp(target + 1);
@@ -25,4 +25,46 @@ std::vector<std::vector<int>> combinationSum2(std::vector<int> candidates, int t
         }
     }
     return dp.back();
+}
+
+std::vector<std::vector<int>> combinationSum2v2(std::vector<int> candidates, int target) {
+    // std::sort(candidates.begin(), candidates.end());
+    std::unordered_map<int, int> map;
+    for (int num : candidates) map[num]++;
+    std::vector<std::vector<std::vector<int>>> dp(target + 1);
+    dp[0].push_back(std::vector<int>());
+    for (auto& [key, value] : map) {
+        for (int i = dp.size() - key - 1; (i >= 0); i--) {
+            if (!dp[i].empty()) {
+                for (std::vector<int> way : dp[i]) {
+                    int  index{ i }, cpy{ value };
+                    while (index + key < dp.size() && cpy) {
+                        way.push_back(key);
+                        dp[index + key].push_back(way);
+                        index += key;
+                        cpy--;
+                    }
+                }
+
+            }
+        }
+    }
+    return dp.back();
+}
+
+std::vector<std::vector<int>> combinationSum2Set(std::vector<int> candidates, int target){
+    //Slower, kind of cheating
+    std::sort(candidates.begin(), candidates.end());
+    std::vector<std::set<std::vector<int>>> dp(target + 1);
+    for (int num : candidates) {
+        if (num >= dp.size()) continue;
+        for (size_t i = dp.size() - num - 1; i > 0; i--) {
+            for (std::vector<int> way : dp[i]) {
+                way.push_back(num);
+                dp[i + num].insert(way);
+            }
+        }
+        dp[num].insert(std::vector(1, num));
+    }
+    return std::vector<std::vector<int>>(dp.back().begin(), dp.back().end());
 }
--- DynamicProgramming/DynamicProgramming/candidateCombSum2.hpp
+++ DynamicProgramming/DynamicProgramming/candidateCombSum2.hpp
@@ -1,4 +1,8 @@
 #pragma once
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
+#include <set>
 std::vector<std::vector<int>> combinationSum2(std::vector<int> candidates, int target);
+std::vector<std::vector<int>> combinationSum2v2(std::vector<int> candidates, int target);//This one now works with LeetCode too
+std::vector<std::vector<int>> combinationSum2Set(std::vector<int> candidates, int target);
