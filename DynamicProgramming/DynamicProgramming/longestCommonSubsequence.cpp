94c2861aec02ab42267d953c1a33c00a13b8fdc1
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-25 11:11:10 +0100

    Update longestCommonSubsequence.cpp
    Correct solution

--- DynamicProgramming/DynamicProgramming/longestCommonSubsequence.cpp
+++ DynamicProgramming/DynamicProgramming/longestCommonSubsequence.cpp
@@ -1,13 +1,15 @@
 #include "longestCommonSubsequence.hpp"
 
 int longestCommonSubsequence(std::string text1, std::string text2) {
-    std::vector<std::vector<int>> dp(text1.length(), std::vector<int>(text2.length()));
+    std::vector<std::vector<int>> dp(text1.length() + 1, std::vector<int>(text2.length() + 1));
     // dp[0][0]=text1[0]==text2[0];
-    for (int i = 0; i < text1.length(); i++) {
-        for (int j = 0; j < text2.length(); j++) {
-            dp[i][j] =std::max((j > 0) ? dp[i][j - 1]:0, (i > 0) ? dp[i - 1][j] : 0);
-            if (text1[i] == text2[j]) {
-                dp[i][j]++;
+    for (int i = 1; i < text1.length() + 1; i++) {
+        for (int j = 1; j < text2.length() + 1; j++) {
+            if (text1[i - 1] == text2[j - 1]) {
+                dp[i][j] += dp[i - 1][j - 1] + 1; //I missed this!!!! The previous subsequence is there
+            }
+            else {
+                dp[i][j] = std::max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
     }
