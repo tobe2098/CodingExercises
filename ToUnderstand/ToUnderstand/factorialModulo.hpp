f72e35e62540c652ebb91a0dc106add0bffa752e
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-27 15:02:06 +0100

    Things to study

---
+++ ToUnderstand/ToUnderstand/factorialModulo.hpp
@@ -0,0 +1,25 @@
+#pragma once
+int factmod(int n, int p) {
+    vector<int> f(p);
+    f[0] = 1;
+    for (int i = 1; i < p; i++)
+        f[i] = f[i - 1] * i % p;
+
+    int res = 1;
+    while (n > 1) {
+        if ((n / p) % 2)
+            res = p - res;
+        res = res * f[n % p] % p;
+        n /= p;
+    }
+    return res;
+}
+def twos(n) :
+    res = 0
+    p = 1
+    for i in range(1, n) :
+        if p > n:
+break
+p *= 2
+res += n // p
+return res
