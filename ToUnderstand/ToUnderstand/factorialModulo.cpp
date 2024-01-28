c1b258bffeb8f8f3953bc02fbcda7878e3930295
Antoni Bertolin <tonibm1220@gmail.com>
2024-01-28 11:17:03 +0100

    Update factorialModulo.cpp
    Explanation

--- ToUnderstand/ToUnderstand/factorialModulo.cpp
+++ ToUnderstand/ToUnderstand/factorialModulo.cpp
@@ -17,6 +17,8 @@ int factmod(int n, int p) {
 }
 
 int noTwosInFactorial(int num) {
+    //This works because when you do num/2, you get all the times you find 2**1 in the numbers between 1 and num.
+    //This expands for the rest of powers of 2, which gets you the number of 2s by summing like that.
     int res{}, p{ 1 };
     for (int i : std::ranges::iota_view{ 1,num }) {
         if (p > num) break;
