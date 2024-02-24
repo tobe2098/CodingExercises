8deaf36d4c3a9cc6cf24dc97861f858903074f25
Antoni Bertolin <tonibm1220@gmail.com>
2024-02-24 15:34:12 +0100

    Formattting

--- Others/Others/secretMeetings.hpp
+++ Others/Others/secretMeetings.hpp
@@ -8,7 +8,7 @@ struct SGraphNode {
     std::unordered_map<int, std::vector<int>> meets_time;
     SGraphNode(int n) { id = n; }
 };
-
+//Problem 2092, go visit the more optimal solutions
 std::vector<int> findAllPeople(int n, std::vector<std::vector<int>>& meetings, int firstPerson);
 
 std::vector<int> findAllPeople2(int n, std::vector<std::vector<int>>& meetings, int firstPerson);
