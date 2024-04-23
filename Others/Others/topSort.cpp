f8fca8769327d7cf5514b148b5cd4d63b04dedbb
Antoni Bertolin <tonibm1220@gmail.com>
2024-04-23 17:42:37 +0200

    New

---
+++ Others/Others/topSort.cpp
@@ -0,0 +1,31 @@
+#include "topSort.hpp"
+
+std::vector<int> findMinHeightTrees(int n, std::vector<std::vector<int>> edges) {
+    //Assume a direction inwards towards the direction of centre of gravity.
+    std::set<int> erased;
+    std::vector<std::set<int>> adj(n);
+    for (int i = 0; i < edges.size(); i++) {
+        int e1{ edges[i][0] };
+        int e2{ edges[i][1] };
+        adj[e1].insert(e2);
+        adj[e2].insert(e1);
+    }
+    while (n - erased.size() > 2) {
+        std::vector<int> to_erase;
+        for (int i = 0; i < n; i++) {
+            if (erased.contains(i))continue;
+            if (adj[i].size() == 1) to_erase.push_back(i);
+        }
+        for (int node : to_erase) {
+            for (std::set<int>& set : adj) {
+                set.erase(node);
+            }
+            erased.insert(node);
+        }
+    }
+    std::vector<int> sol;
+    for (int i = 0; i < n; i++) {
+        if (!erased.contains(n)) sol.push_back(i);
+    }
+    return sol;
+}
---
+++ Others/Others/topSort.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <vector>
+#include <set>
+std::vector<int> findMinHeightTrees(int n, std::vector<std::vector<int>> edges);
