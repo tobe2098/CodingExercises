44e453c316ee90b2389a2fdc89c6190be4449ae8
Antoni Bertolin <tonibm1220@gmail.com>
2024-06-19 17:38:43 +0200

    New

--- Others/Others/binSearch.cpp
+++ Others/Others/binSearch.cpp
@@ -27,3 +27,38 @@ bool searchMatrix(std::vector<std::vector<int>> matrix, int target) {
     if (matrix[index][p1] == target)return true;
     return false;
 }
+
+int bouquets(std::vector<int>& bloomDay, int k, int day) {
+    int cnt{}, num{};
+    for (int d : bloomDay) {
+        if (d <= day) {
+            cnt++;
+            if (cnt == k) {
+                num++;
+                cnt = 0;
+            }
+        }
+        else {
+            cnt = 0;
+        }
+    }
+    return num;
+}
+
+int minDays(std::vector<int> bloomDay, int m, int k) {
+    if (static_cast<long>(m) * k > bloomDay.size()) return -1;
+    int max{ *std::max_element(bloomDay.begin(), bloomDay.end()) }, min{ *std::min_element(bloomDay.begin(), bloomDay.end()) }, f{ min };
+    std::vector<int> visited(max - min+1, -1);
+    while (min < max) {
+        int mid{ min + (max - min) / 2 };
+        int b{ (visited[mid - f] == -1) ? bouquets(bloomDay, k, mid) : visited[mid - f] };
+        visited[mid - f] = b;
+        if (b >= m) {
+            max = mid;
+        }
+        else if (b < m) {
+            min = mid + 1;
+        }
+    }
+    return min;
+}
--- Others/Others/binSearch.hpp
+++ Others/Others/binSearch.hpp
@@ -1,3 +1,5 @@
 #pragma once
 #include <vector>
 bool searchMatrix(std::vector<std::vector<int>> matrix, int target);
+int bouquets(std::vector<int>& bloomDay, int k, int day);
+int minDays(std::vector<int> bloomDay, int m, int k);
