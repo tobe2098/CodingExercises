70873ff4ffbbbc18aa14c62cca9816ad3d2a07b6
Antoni Bertolin <tonibm1220@gmail.com>
2024-07-04 11:35:05 +0200

    UnionFind data structure in C

---
+++ DataStructures/UnionFind.c
@@ -0,0 +1,64 @@
+#include "UnionFind.h"
+
+int UnionFindByRank_alloc(UnionFindByRank *unionfind, int size) {
+  int *parents = (int *)malloc(size * sizeof(int));
+  int *rank    = (int *)malloc(size * sizeof(int));
+  if (!(rank && parents)) {
+    return -1;
+  }
+  for (int i = 0; i < size; i++) {
+    parents[i] = i;
+    rank[i]    = 1;
+  }
+  unionfind->parents = parents;
+  unionfind->rank    = rank;
+  unionfind->size    = size;
+  return 0;
+}
+
+int UnionFindByRank_free(UnionFindByRank *unionfind) {
+  if (!unionfind) {
+    return -1;
+  }
+  free(unionfind->parents);
+  free(unionfind->rank);
+  unionfind->size = 0;
+  return 0;
+}
+
+int UnionFindByRank_find(UnionFindByRank *unionfind, int child) {
+  if (!unionfind || child >= unionfind->size || child < 0) {
+    return -1;
+  }
+  if (unionfind->parents[child] != child) {
+    unionfind->parents[child] = UnionFindByRank_find(unionfind, unionfind->parents[child]);
+  }
+  return unionfind->parents[child];
+}
+
+int UnionFindByRank_merge(UnionFindByRank *unionfind, int x, int y) {
+  if (!unionfind || unionfind->size <= x || unionfind->size <= y || x < 0 || y < 0) {
+    return -1;
+  }
+  int parent_x = UnionFindByRank_find(unionfind, x);
+  int parent_y = UnionFindByRank_find(unionfind, y);
+  int rank_x   = unionfind->rank[parent_x];
+  int rank_y   = unionfind->rank[parent_y];
+  if (rank_x > rank_y) {
+    unionfind->parents[parent_y] = parent_x;
+  } else if (rank_y > rank_x) {
+    unionfind->parents[parent_x] = parent_y;
+  } else {
+    unionfind->parents[parent_y] = parent_x;
+    unionfind->rank[parent_x]++;
+  }
+  return 0;
+}
+
+int UnionFindByRank_size(UnionFindByRank *unionfind) {
+  if (unionfind) {
+    return unionfind->size;
+  } else {
+    return -1;
+  }
+}
---
+++ DataStructures/UnionFind.h
@@ -0,0 +1,18 @@
+#ifndef _UNION_FIND_HEADER_
+#define _UNION_FIND_HEADER_
+
+typedef struct {
+    /* data */
+    int* parents;
+    int* rank;
+    int  size;
+} UnionFindByRank;
+
+int UnionFindByRank_alloc(UnionFindByRank* unionfind, int size);
+
+int UnionFindByRank_free(UnionFindByRank* unionfind);
+int UnionFindByRank_find(UnionFindByRank* unionfind, int child);
+int UnionFindByRank_merge(UnionFindByRank* unionfind, int x, int y);
+int UnionFindByRank_size(UnionFindByRank* unionfind);
+
+#endif
